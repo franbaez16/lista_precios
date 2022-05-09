3123cec59d249e6b3d6e53b6acedfa8ffa873045
Fran Baez <103434629+franbaez16@users.noreply.github.com>
2022-05-09 12:48:41 -0300

    Add files via upload

---
+++ lista_precio.js
@@ -0,0 +1,27 @@
+var respuesta; 
+
+alert ("Lista de precios")
+alert("Papas, Coca cola, chocolate en barra, galletas Mana, Sprite. ¡¡¡¡AL ESCRIBIR EL NOMBRE DEL PRODUCTO, ESCRIBIRLO TAL CUAL COMO ESTA AQUIIIII!!!!")
+
+respuesta=prompt("¿De que producto quiere saber el precio?")
+
+if (respuesta == "Papas")
+{
+    alert("$100")
+}
+if (respuesta == "Coca Cola") 
+{
+    alert("$150 500ml; $290 1lt")
+}
+if (respuesta == "chocolate en barra")
+{
+    alert("$100")
+}
+if (respuesta == "galletas Mana")
+{
+    alert("$80")
+}
+if (respuesta == "Sprite")
+{
+    alert("$120 500ml; $290 1lt")
+ }
